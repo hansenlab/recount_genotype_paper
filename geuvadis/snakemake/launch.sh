3ed9cfd125ae1f9fc09efde8eefae1730f896f89
Chris Lo <chris.aa.lo@gmail.com>
2023-06-10 17:42:45 -0400

    Delete geuvadis/snakemake directory

--- geuvadis/snakemake/launch.sh
+++
@@ -1,1 +0,0 @@
-snakemake --cluster-sync "qsub -sync yes -l mem_free=50G -l h_vmem=50G -l h_fsize=100G -e /users/clo/recount_genotype/geuvadis -o /users/clo/recount_genotype/geuvadis" --jobs 100 --latency-wait 60
